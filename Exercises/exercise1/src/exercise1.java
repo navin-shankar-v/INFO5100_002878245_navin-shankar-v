575c90a8d394ec46c3944452383a234a8eb6c1dc
navin-shankar-v <gvnavinshankar@gmail.com>
2023-10-14 13:00:40 -0700

    Created module excercise1

---
+++ Exercises/exercise1/src/exercise1.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class exercise1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hai");
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+
+        }
+    }
